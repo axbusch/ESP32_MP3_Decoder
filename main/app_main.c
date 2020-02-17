762bc95e33939a89000d9172619c083b94b44127
funky-home\funky <axelbusch@gmx.de>
2020-02-17 22:52:52 +0100

    format

--- main/app_main.c
+++ main/app_main.c
@@ -1,96 +1,95 @@
 
 #include "app_main.h"
-#define WIFI_LIST_NUM   10
+#define WIFI_LIST_NUM 10
 
 
 #define TAG "main"
 
 
-//Priorities of the reader and the decoder thread. bigger number = higher prio
-#define PRIO_READER configMAX_PRIORITIES -3
+// Priorities of the reader and the decoder thread. bigger number = higher prio
+#define PRIO_READER configMAX_PRIORITIES - 3
 #define PRIO_MQTT configMAX_PRIORITIES - 3
-#define PRIO_CONNECT configMAX_PRIORITIES -1
-
+#define PRIO_CONNECT configMAX_PRIORITIES - 1
 
 
 static void init_hardware()
 {
-    nvs_flash_init();
+  nvs_flash_init();
 
-    // init UI
-    // ui_init(GPIO_NUM_32);
+  // init UI
+  // ui_init(GPIO_NUM_32);
 
-    //Initialize the SPI RAM chip communications and see if it actually retains some bytes. If it
-    //doesn't, warn user.
-    if (!spiRamFifoInit()) {
-        printf("\n\nSPI RAM chip fail!\n");
-        while(1);
-    }
+  // Initialize the SPI RAM chip communications and see if it actually retains some bytes. If it
+  // doesn't, warn user.
+  if (!spiRamFifoInit()) {
+    printf("\n\nSPI RAM chip fail!\n");
+    while (1)
+      ;
+  }
 
-    ESP_LOGI(TAG, "hardware initialized");
+  ESP_LOGI(TAG, "hardware initialized");
 }
 
 static void start_wifi()
 {
-    ESP_LOGI(TAG, "starting network");
+  ESP_LOGI(TAG, "starting network");
 
-    /* FreeRTOS event group to signal when we are connected & ready to make a request */
-    EventGroupHandle_t wifi_event_group = xEventGroupCreate();
+  /* FreeRTOS event group to signal when we are connected & ready to make a request */
+  EventGroupHandle_t wifi_event_group = xEventGroupCreate();
 
-    /* init wifi */
-    ui_queue_event(UI_CONNECTING);
-    initialise_wifi(wifi_event_group);
+  /* init wifi */
+  ui_queue_event(UI_CONNECTING);
+  initialise_wifi(wifi_event_group);
 
-    /* Wait for the callback to set the CONNECTED_BIT in the event group. */
-    xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
-                        false, true, portMAX_DELAY);
+  /* Wait for the callback to set the CONNECTED_BIT in the event group. */
+  xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
 
-    ui_queue_event(UI_CONNECTED);
+  ui_queue_event(UI_CONNECTED);
 }
 
-static renderer_config_t *create_renderer_config()
+static renderer_config_t* create_renderer_config()
 {
-    renderer_config_t *renderer_config = calloc(1, sizeof(renderer_config_t));
+  renderer_config_t* renderer_config = calloc(1, sizeof(renderer_config_t));
 
+  renderer_config->bit_depth = I2S_BITS_PER_SAMPLE_16BIT;
+  renderer_config->i2s_num = I2S_NUM_0;
+  renderer_config->sample_rate = 44100;
+  renderer_config->sample_rate_modifier = 1.0;
+  renderer_config->output_mode = AUDIO_OUTPUT_MODE;
+
+  if (renderer_config->output_mode == I2S_MERUS) {
+    renderer_config->bit_depth = I2S_BITS_PER_SAMPLE_32BIT;
+  }
+
+  if (renderer_config->output_mode == DAC_BUILT_IN) {
     renderer_config->bit_depth = I2S_BITS_PER_SAMPLE_16BIT;
-    renderer_config->i2s_num = I2S_NUM_0;
-    renderer_config->sample_rate = 44100;
-    renderer_config->sample_rate_modifier = 1.0;
-    renderer_config->output_mode = AUDIO_OUTPUT_MODE;
+  }
 
-    if(renderer_config->output_mode == I2S_MERUS) {
-        renderer_config->bit_depth = I2S_BITS_PER_SAMPLE_32BIT;
-    }
-
-    if(renderer_config->output_mode == DAC_BUILT_IN) {
-        renderer_config->bit_depth = I2S_BITS_PER_SAMPLE_16BIT;
-    }
-
-    return renderer_config;
+  return renderer_config;
 }
 
- void start_web_radio()
+void start_web_radio()
 {
-    // init web radio
-    web_radio_t *radio_config = calloc(1, sizeof(web_radio_t));
-    radio_config->playlist = playlist_create();
-    playlist_load_pls(radio_config->playlist);
+  // init web radio
+  web_radio_t* radio_config = calloc(1, sizeof(web_radio_t));
+  radio_config->playlist = playlist_create();
+  playlist_load_pls(radio_config->playlist);
 
 
-    // init player config
-    radio_config->player_config = calloc(1, sizeof(player_t));
-    radio_config->player_config->command = CMD_NONE;
-    radio_config->player_config->decoder_status = UNINITIALIZED;
-    radio_config->player_config->decoder_command = CMD_NONE;
-    radio_config->player_config->buffer_pref = BUF_PREF_SAFE;
-    radio_config->player_config->media_stream = calloc(1, sizeof(media_stream_t));
+  // init player config
+  radio_config->player_config = calloc(1, sizeof(player_t));
+  radio_config->player_config->command = CMD_NONE;
+  radio_config->player_config->decoder_status = UNINITIALIZED;
+  radio_config->player_config->decoder_command = CMD_NONE;
+  radio_config->player_config->buffer_pref = BUF_PREF_SAFE;
+  radio_config->player_config->media_stream = calloc(1, sizeof(media_stream_t));
 
-    // init renderer
-    renderer_init(create_renderer_config());
+  // init renderer
+  renderer_init(create_renderer_config());
 
-    // start radio
-    web_radio_init(radio_config);
-    web_radio_start(radio_config);
+  // start radio
+  web_radio_init(radio_config);
+  web_radio_start(radio_config);
 }
 
 /**
@@ -98,18 +97,18 @@ static renderer_config_t *create_renderer_config()
  */
 void app_main()
 {
-    ESP_LOGI(TAG, "starting app_main()");
-    ESP_LOGI(TAG, "RAM left: %u", esp_get_free_heap_size());
+  ESP_LOGI(TAG, "starting app_main()");
+  ESP_LOGI(TAG, "RAM left: %u", esp_get_free_heap_size());
 
-    init_hardware();
+  init_hardware();
 
 #ifdef CONFIG_BT_SPEAKER_MODE
-    bt_speaker_start(create_renderer_config());
+  bt_speaker_start(create_renderer_config());
 #else
-    start_wifi();
-    start_web_radio();
+  start_wifi();
+  start_web_radio();
 #endif
 
-    ESP_LOGI(TAG, "RAM left %d", esp_get_free_heap_size());
-    // ESP_LOGI(TAG, "app_main stack: %d\n", uxTaskGetStackHighWaterMark(NULL));
+  ESP_LOGI(TAG, "RAM left %d", esp_get_free_heap_size());
+  // ESP_LOGI(TAG, "app_main stack: %d\n", uxTaskGetStackHighWaterMark(NULL));
 }
