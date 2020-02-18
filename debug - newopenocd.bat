470eff5a19d331ba5e465cbe45eb44c5e51b16ee
funky-home\funky <axelbusch@gmx.de>
2020-02-18 22:06:37 +0100

    *****

---
+++ debug - newopenocd.bat
@@ -0,0 +1,5 @@
+call D:\esp\idf\export.bat
+
+set OPENOCD_SCRIPTS=D:\esp\toolchain\.espressif\tools\openocd-esp32\v0.10.0-esp32-20191114\openocd-esp32\share\openocd\scripts
+set PATH=D:\esp\toolchain\.espressif\tools\openocd-esp32\v0.10.0-esp32-20191114\openocd-esp32\bin;%PATH%
+openocd.exe   -f board/esp32-wrover-kit-3.3v.cfg
