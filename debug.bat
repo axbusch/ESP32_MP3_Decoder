2908bbc7cfa6b8fef203badaa96eaa95dc7a3f1d
funky-home\funky <axelbusch@gmx.de>
2020-02-24 20:53:30 +0100

    ...

--- debug.bat
+++ debug.bat
@@ -1,3 +1,5 @@
 call D:\esp\idf\export.bat
 
-openocd.exe  -f interface/ftdi/esp32_devkitj_v1.cfg -f board/esp32-wrover.cfg
+set OPENOCD_SCRIPTS=D:\esp\toolchain\.espressif\tools\openocd-esp32\v0.10.0-esp32-20191114\openocd-esp32\share\openocd\scripts
+set PATH=D:\esp\toolchain\.espressif\tools\openocd-esp32\v0.10.0-esp32-20191114\openocd-esp32\bin;%PATH%
+openocd.exe   -f board/esp32-wrover-kit-3.3v.cfg
